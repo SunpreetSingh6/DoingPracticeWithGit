68a67c0ee4c904cb2aaffcfd5ad67033fde33059
Sunpreet Singh <ssunpreet1999@gmail.com>
2022-05-27 18:13:46 +0530

    Add a line in sum.java file at line no. 6

--- Sum.java
+++ Sum.java
@@ -3,6 +3,7 @@ class Sum
     public static void main(String args[])
     {
         int a = 5 , b = 10;
+        System.out.println("Program to print sum of two numbers");
         System.out.println("The sum of a and b is :- " + (a+b) );
     }
 }
