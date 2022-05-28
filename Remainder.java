fb8c4c92e8f8939dfca5c55561c53a551e654085
Sunpreet Singh <ssunpreet1999@gmail.com>
2022-05-28 17:34:07 +0530

    Added few lines and remove one line from Remainder.java

--- Remainder.java
+++ Remainder.java
@@ -3,7 +3,8 @@ class Sum
     public static void main(String args[])
     {
         int a = 3 , b = 10;
-        System.out.println("Program to print sum of two numbers");
+        System.out.println("Java Remainder Program");
+        System.out.println("Program to print remainder of two numbers");
         System.out.println("The sum of a and b is :- " + (b%a) );
     }
 }
