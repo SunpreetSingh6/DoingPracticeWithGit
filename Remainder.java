3d2485c8db7d3bfd93a6faf7781fef3b35d64b3a
Sunpreet Singh <ssunpreet1999@gmail.com>
2022-05-28 17:27:50 +0530

    Added a file Remainder.java

---
+++ Remainder.java
@@ -0,0 +1,9 @@
+class Sum
+{
+    public static void main(String args[])
+    {
+        int a = 3 , b = 10;
+        System.out.println("Program to print sum of two numbers");
+        System.out.println("The sum of a and b is :- " + (b%a) );
+    }
+}
