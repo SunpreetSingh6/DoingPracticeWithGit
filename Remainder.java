c270838893a7df0914b517afdee105ee022e77a1
Sunpreet Singh <ssunpreet1999@gmail.com>
2022-05-28 20:44:38 +0530

    Removed a line and storing result into a variable

--- Remainder.java
+++ Remainder.java
@@ -3,8 +3,8 @@ class Sum
     public static void main(String args[])
     {
         int a = 3 , b = 10;
-        System.out.println("Java Remainder Program");
+        c = b % a; 
         System.out.println("Program to print remainder of two numbers");
-        System.out.println("The sum of a and b is :- " + (b%a) );
+        System.out.println("The sum of a and b is :- " + c );
     }
 }
